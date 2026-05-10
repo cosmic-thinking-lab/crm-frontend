e71fa1352168ffe32acd0e864087d672adf40b7a
Punam Prajapati <punamprajapati1012@gmail.com>
2026-05-10 21:18:14 +0530

    added env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api/v1', // Updated with versioning
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
